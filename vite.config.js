cfe5fb782a91df8f8f7e5c9ec45e934658a8be0d
xruzyyy <cruzjerome012@gmail.com>
2024-06-16 20:27:03 +0800

    check for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: [
+        'resources/sass/app.scss',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+  ],
+  build: {
+    outDir: 'public/build', // Adjusted output directory to match Laravel's public directory
+    // You can customize further build options if needed
+  },
 });
